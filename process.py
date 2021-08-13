91190bffadf4420b173303c7a0e18b265cff30ef
Clint Edwards <cedwards1080@gmail.com>
2021-08-13 10:15:44 -0600

    finished part 2

--- process.py
+++ process.py
@@ -1,12 +1,12 @@
-log_file = open("um-server-01.txt")
+log_file = open("um-server-01.txt") # This line brings in the data to our python script so we can manipulate it
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file): # This is like creating a "View" or "Report" to view the data 
+    for line in log_file: # Start of the for loop 
+        line = line.rstrip() # rstrip is a methon that removes trailing characters that you specify in the argument like when we rstrip( \n)
+        day = line[0:3] # This is setting day equal to the first 3 characters of the line starting at zero.
+        if day == "Mon": # This is the condition
+            print(line) # This prints out the line if condition is met
 
 
-sales_reports(log_file)
+sales_reports(log_file) # Calling the function, basically running the report. 
